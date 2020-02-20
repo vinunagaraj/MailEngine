8c1ffc934b56972f4a62e3d2b69dc9c144cfe21c
Abulais <raghibahsan.ahsan@gmail.com>
2020-02-20 08:25:37 +0000

    Raghib - First Commit

---
+++ src/main/java/com/geekyants/mailengine/config/SpringBusinessConfig.java
@@ -0,0 +1,14 @@
+package com.geekyants.mailengine.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class SpringBusinessConfig {
+
+	public SpringBusinessConfig() {
+		super();
+	}
+
+}
--- src/main/java/com/geekyants/mailengine/controller/CustomerController.java
+++ src/main/java/com/geekyants/mailengine/controller/CustomerController.java
@@ -1,9 +1,19 @@
 package com.geekyants.mailengine.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+	
+	
+	
+	
+	
+	
 	
 
 }
